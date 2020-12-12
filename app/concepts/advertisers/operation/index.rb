caeb96bbb0aaac8dae8470481239ce21c6678318
panicboat <panicboat@gmail.com>
2020-12-12 18:36:46 +0900

    Implements filter

--- app/concepts/advertisers/operation/index.rb
+++ app/concepts/advertisers/operation/index.rb
@@ -4,11 +4,12 @@ module Advertisers::Operation
     step Contract::Build(constant: Advertisers::Contract::Index)
     step Contract::Validate()
     fail :invalid_params!
+    step :permit!
     step :model
 
     def model(ctx, **)
       contract = ctx[:"contract.default"]
-      data = ::Advertiser.paging(contract.limit, contract.offset).order(contract.order)
+      data = filter(ctx, ::Advertiser, 'id').paging(contract.limit, contract.offset).order(contract.order)
       data = data.where({ agency_id: contract.agency_id }) if contract.agency_id.present?
       ctx[:model] = OpenStruct.new({ Advertisers: data })
     end
