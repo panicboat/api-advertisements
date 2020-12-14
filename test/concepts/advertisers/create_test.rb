c8ccf55bd496ab1e1790ddf8b625f24512f4f000
panicboat <panicboat@gmail.com>
2020-12-14 22:15:58 +0900

    Rename iam_get_permission.json

--- test/concepts/advertisers/create_test.rb
+++ test/concepts/advertisers/create_test.rb
@@ -5,7 +5,7 @@ module Advertisers
     setup do
       @current_user = JSON.parse({ name: 'Spec' }.to_json, object_class: OpenStruct)
       WebMock.stub_request(:get, "#{ENV['HTTP_IAM_URL']}/permissions/").to_return(
-        body: File.read("#{Rails.root}/test/fixtures/files/iam_get_permission.json"),
+        body: File.read("#{Rails.root}/test/fixtures/files/platform_iam_get_permission.json"),
         status: 200,
         headers: { "Content-Type": 'application/json' }
       )
