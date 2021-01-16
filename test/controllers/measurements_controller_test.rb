27193930ed835eb3350ab06575b2a1ee7278571e
panicboat <panicboat@gmail.com>
2021-01-16 21:48:56 +0900

    Fix test cases

--- test/controllers/measurements_controller_test.rb
+++ test/controllers/measurements_controller_test.rb
@@ -14,6 +14,16 @@ class MeasurementsControllerTest < ActionDispatch::IntegrationTest
       status: 200,
       headers: { 'Content-Type': 'application/json' }
     )
+    WebMock.stub_request(:get, %r{#{ENV['HTTP_IAM_URL']}/services/#{ENV['PNB_SERVICE_ID']}/actions}).to_return(
+      body: File.read("#{Rails.root}/test/fixtures/files/platform_iam_list_action.json"),
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    )
+    WebMock.stub_request(:get, %r{#{ENV['HTTP_IAM_URL']}/permissions/}).to_return(
+      body: File.read("#{Rails.root}/test/fixtures/files/platform_iam_get_permission.json"),
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    )
     @headers = { "#{::RequestHeader::USER_CLAIMS}": 'dummy' }
   end
 
